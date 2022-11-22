2640a3ae2e9eff964d5cd2805ec52d4e2e88a1d3
Rajat Saha <rajat.12.saha1995@gmail.com>
2022-11-22 16:03:29 +0530

    updating the Css file and Error validation message

--- src/Validation.js
+++ src/Validation.js
@@ -1,7 +1,18 @@
 import * as yup from "yup";
 
 export const Myvalidation = yup.object().shape({
-  email: yup.string().email("Please Enter Valid Email Id").required("Email id is Required"),
-  firstName: yup.string().matches(/^[A-Z a-z]*$/, 'Please enter a valid First name').min(3, "Please enter a First name more than 3 character").required("quantity is Required"),
-  lastName: yup.string().matches(/^[A-Z a-z]*$/, 'Please enter a valid Last name').min(3, "Please enter a Last name more than 3 character").required("price Name is Required"),
+  email: yup
+    .string()
+    .email("Please Enter Valid Email Id")
+    .required("Email id is Required"),
+  firstName: yup
+    .string()
+    .matches(/^[A-Z a-z]*$/, "Please enter a valid First name")
+    .min(3, "Please enter a First name more than 3 character")
+    .required("First Name is Required"),
+  lastName: yup
+    .string()
+    .matches(/^[A-Z a-z]*$/, "Please enter a valid Last name")
+    .min(3, "Please enter a Last name more than 3 character")
+    .required("Last Name is Required"),
 });
